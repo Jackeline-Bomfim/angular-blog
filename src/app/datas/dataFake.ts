46aae78b93cd249fd76e093139974dcb47fd557c
Jackeline-Bomfim <kelinehta12@gmail.com>
2022-11-27 18:53:35 -0300

    atualização do conteúdo

---
+++ src/app/datas/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+        "description": "MARVEL anuncia novo filme do homem de ferro.",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX3im72_yGUUga-pVFR8To-f9NpUTeR0K-yA&usqp=CAU"
+    },
+    {
+        "id": "2",
+        "title": "Saiu a nova versão do Angular",
+        "description": "Olá mundo",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Nova serie anunciada na Disney+",
+        "description": "Olá mundo",
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/08/WandaVision-Falcao-e-Loki-1024x576.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Saiba tudo sobre as series da MARVEL",
+        "description": "Olá mundo",
+        "photoCover": "https://t2.tudocdn.net/553407?w=646&h=284"
+    }
+]
